4580a35f0fc87f250948a8bac5db9adf41255ef9
witehound <netvid00@gmail.com>
2023-03-24 13:54:02 +0100

    added price struct andd implementation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ struct Order {
     order_type: BidOrAsk,
 }
 
+struct Price {
+    integral: u64,
+    fractional: u64,
+    scaler: u64,
+}
+
+struct Limit {
+    price: Price,
+    oredr: Vec<Order>,
+}
+
 enum BidOrAsk {
     Bid,
     Ask,
@@ -11,6 +22,19 @@ enum BidOrAsk {
 impl Order {
     fn new(size: f64, order_type: BidOrAsk) -> Order {
         Order { size, order_type }
+    }
+}
+
+impl Price {
+    fn new(price: f64) -> Price {
+        let scaler: u64 = 100000;
+        let integral = price as u64;
+        let fractional = ((price % 1.0) * scaler as f64) as u64;
+        Price {
+            integral,
+            fractional,
+            scaler,
+        }
     }
 }
 
