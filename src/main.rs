cb0cc9de098e6c69dc73b1734436e55979fa5426
witehound <netvid00@gmail.com>
2023-03-24 13:42:03 +0100

    added order and bidorask enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Order {
+    size: f64,
+    order_type: BidOrAsk,
+}
+
+enum BidOrAsk {
+    Bid,
+    Ask,
+}
+
+impl Order {
+    fn new(size: f64, order_type: BidOrAsk) -> Order {
+        Order { size, order_type }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
